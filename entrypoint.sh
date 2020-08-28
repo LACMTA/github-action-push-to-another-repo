3ab5c532e0865fdbe2be2213bdc9a2de875b5678
albertkun <albertk@gmx.com>
2020-08-27 18:01:29 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,15 @@ echo "Cloning destination git repository"
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
-git clone --single-branch --branch master "https://$API_TOKEN_GITHUB@github.com/$REPO_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
+git clone --single-branch --branch master -filter=blob:none --no-checkout "https://$API_TOKEN_GITHUB@github.com/$REPO_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
+
+cd data/
+git sparse-checkout init --cone
+git sparse-checkout set data/
 ls -la "$CLONE_DIR"
+
+
+
 
 echo "Cleaning destination repository of old files"
 # Copy files into the git and deletes all git
