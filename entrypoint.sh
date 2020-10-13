a3f2bedc3754a7b3216c750bbb706078b99f5109
albertkun <albertk@gmx.com>
2020-10-12 17:33:01 -0700

    updated sh for date time
    added better feature

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ GITHUB_USERNAME="$2"
 GITHUB_REPO="$3"
 USER_EMAIL="$4"
 REPO_USERNAME="$5"
-DATESTAMP = date +"%D"
+DATESTAMP=`date +"%D"`
 if [ -z "$REPO_USERNAME" ]
 then
   REPO_USERNAME=$GITHUB_USERNAME
@@ -39,7 +39,7 @@ git add data/
 
 echo "$DATESTAMP"
 echo "Adding git commit"
-git commit -m "Update from https://github.com/$GITHUB_REPOSITORY/ on $DATESTAMP"
+  git commit -m "Update from https://github.com/$GITHUB_REPOSITORY/ on $DATESTAMP"
 echo "Pushing git commit"
 git push
 
