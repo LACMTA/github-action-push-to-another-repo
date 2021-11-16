7748c808d35020ade6ea58bf077680e1cddc31c1
albertkun <albertk@gmx.com>
2021-11-15 17:40:10 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@
 echo "Starts"
 API_TOKEN="$1"
 
-
 echo "cleaning tmp folders"
+
+GITHUB_USERNAME="$1"
+USER_EMAIL="$3"
 
 rm -rf mybus
 
@@ -12,9 +14,11 @@ echo "Cloning destination git repository"
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
-git clone --single-branch --branch dev "https://$API_TOKEN@github.com/LACMTA/mybus.git" "mybus"
+git clone --single-branch --branch dev "https://$API_TOKEN_GITHUB@github.com/LACMTA/mybus.git" "mybus"
 
-git remote add mybus-dev "https://$API_TOKEN@github.com/LACMTA/mybus-dev.git"
+git remote add mybus-dev "https://$API_TOKEN_GITHUB@github.com/LACMTA/mybus-dev.git"
 git push mybus-dev dev
 
 echo "Done"
+
+
