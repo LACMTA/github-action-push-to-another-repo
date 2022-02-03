04ee5b9fdb4ac19d1d18f1e2849698e77b0ffc23
Nina Kin <matikin9@gmail.com>
2022-02-02 18:48:12 -0800

    print env variables from calling github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,9 @@ echo "Cloning source repository"
 # Setup git
 git clone --single-branch --branch dev "https://$API_TOKEN_GITHUB@github.com/LACMTA/mybus.git" "mybus"
 
+echo $DESTINATION_GITHUB_USERNAME
+echo $DESTINATION_GITHUB_EMAIL
+
 echo "Adding destination repository as remote"
 git remote add mybus-dev "https://$API_TOKEN_GITHUB@github.com/LACMTA/mybus-dev.git"
 
