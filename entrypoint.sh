39dd708a00e7d2e8f684443b0ee80482e684d199
Nina Kin <matikin9@gmail.com>
2022-02-02 18:50:59 -0800

    fix echo command syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,8 @@ echo "Cloning source repository"
 # Setup git
 git clone --single-branch --branch dev "https://$API_TOKEN_GITHUB@github.com/LACMTA/mybus.git" "mybus"
 
-echo $DESTINATION_GITHUB_USERNAME
-echo $DESTINATION_GITHUB_EMAIL
+echo "$DESTINATION_GITHUB_USERNAME"
+echo "$DESTINATION_GITHUB_EMAIL"
 
 echo "Adding destination repository as remote"
 git remote add mybus-dev "https://$API_TOKEN_GITHUB@github.com/LACMTA/mybus-dev.git"
