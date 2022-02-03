3922a5bd63c276ab2bcc60001927d5a335372634
albertkun <albertk@gmx.com>
2022-02-02 17:36:00 -0800

    feat: added git-config

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,10 @@ rm -rf mybus
 
 echo "Cloning destination git repository"
 # Setup git
+
+git config --global.name $GITHUB_USERNAME
+git config --global user.email $USER_EMAIL
+
 git clone --single-branch --branch dev "https://$METRO_GITHUB_TOKEN@github.com/LACMTA/mybus.git" "mybus"
 
 git remote add mybus-dev "https://$METRO_GITHUB_TOKEN@github.com/LACMTA/mybus-dev.git"
