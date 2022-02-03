5def3fdc9f8dc2d0019ad1c1f602692254a562e9
Nina Kin <matikin9@gmail.com>
2022-02-02 18:42:51 -0800

    added more logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,14 @@ echo "cleaning tmp folders"
 rm -rf mybus
 
 
-echo "Cloning destination git repository"
+echo "Cloning source repository"
 # Setup git
 git clone --single-branch --branch dev "https://$API_TOKEN_GITHUB@github.com/LACMTA/mybus.git" "mybus"
 
+echo "Adding destination repository as remote"
 git remote add mybus-dev "https://$API_TOKEN_GITHUB@github.com/LACMTA/mybus-dev.git"
+
+echo "Pushing to destination repository"
 git push mybus-dev dev
 
 
