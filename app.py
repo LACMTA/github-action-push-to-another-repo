ce34b1fe8747fafb7d2390fbde26235e4a502029
albertkun <albertk@gmx.com>
2022-02-03 18:37:26 -0800

    app.py push

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+from cmath import e
 import os
 import sys
 
-def push_to_github(arg1,arg2):
-	print(arg1)
-	print(arg2)
+def push_to_github(arg3):
+	# print(arg1)
+	# print(arg2)
+	try:
+		os.system('git clone --single-branch --branch dev "https://'+arg3+'@github.com/LACMTA/mybus.git" "mybus"')
+		os.system('git remote add mybus-dev "https://'+arg3+'@github.com/LACMTA/mybus-dev.git"')
+		os.system('git push mybus-dev dev')
+		print('Successfully pushed to mybus-dev')
+	except e as error:
+		print(error)
+		print('Error: Could not clone repository.')
+		sys.exit(1)
 	# os.system('git pull')
 	# os.system('git add .')
 	# os.system('git commit -m "Auto update"')
@@ -13,6 +23,7 @@ def push_to_github(arg1,arg2):
 def __main__():
     inputArg1 = sys.argv[1]
     inputArg2 = sys.argv[2]
-    push_to_github(inputArg1, inputArg2)
+    inputArg3 = sys.argv[3]
+    push_to_github(inputArg3)
     return
 __main__()
