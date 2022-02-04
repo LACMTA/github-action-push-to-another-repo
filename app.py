3fba17a8c0b989742405aad764bf4190b5f25244
albertkun <albertk@gmx.com>
2022-02-03 19:17:47 -0800

    re-added parameters

--- app.py
+++ app.py
@@ -8,29 +8,31 @@ def push_to_github(arg3):
 		os.system('git remote add mybus-dev "https://'+arg3+'@github.com/LACMTA/mybus-dev.git"')
 		os.system('git push mybus-dev dev')
 		print('Successfully pushed to mybus-dev')
-	except Exception as e:
-		print(e)
+	except e as error:
+		print(error)
 		print('Error: Could not clone repository.')
 		sys.exit(1)
+	# os.system('git pull')
+	# os.system('git add .')
+	# os.system('git commit -m "Auto update"')
+	# os.system('git push')
 	return
 
-# def main():
-# #    gh_key = ''
-# #    try:
-# #       opts, args = getopt.getopt(argv,"hk:",["key="])
-# #    except getopt.GetoptError:
-# #       print ('app.py -k <key>')
-# #       sys.exit(2)
-# #    for opt, arg in opts:
-# #       if opt == '-h':
-# #          print ('app.py -k <key>')
-# #          sys.exit()
-# #       elif opt in ("-k", "--key"):
-# #          gh_key = arg
-#    push_to_github(gh_key)
-#    return
+def main(argv):
+   gh_key = ''
+   try:
+      opts, args = getopt.getopt(argv,"hk:",["key="])
+   except getopt.GetoptError:
+      print ('app.py -k <key>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print ('app.py -k <key>')
+         sys.exit()
+      elif opt in ("-k", "--key"):
+         gh_key = arg
+   push_to_github(gh_key)
+   return
 
 if __name__ == "__main__":
-	gh_key = os.environ.get('API_TOKEN_GITHUB')
-	push_to_github(gh_key)
-	print('Done.')
+   main(sys.argv[1:])
